1c27862a55a978e3b461226187defa9de2f5be70
kshavonina <kshavonina@yandex.ru>
2017-10-07 18:48:40 +0300

    Delete empty constructor

--- src/main/java/com/epam/spring/core/course/loggers/ConsoleEventLogger.java
+++ src/main/java/com/epam/spring/core/course/loggers/ConsoleEventLogger.java
@@ -3,11 +3,7 @@ package com.epam.spring.core.course.loggers;
 import com.epam.spring.core.course.bean.Event;
 
 public class ConsoleEventLogger implements EventLogger {
-    public ConsoleEventLogger() {
-
-    }
-
-    public void logEvent(Event event) {
+     public void logEvent(Event event) {
         System.out.println(event.toString());
     }
 }
